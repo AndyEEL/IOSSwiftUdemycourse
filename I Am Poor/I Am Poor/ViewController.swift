e4362e1d29076c9dea1405b310c940d2b71da78a
Andy_july_happy <68048741+AndyEEL@users.noreply.github.com>
2021-02-09 16:32:01 +0900

    Add files via upload

---
+++ I Am Poor/I Am Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by 이동건 on 2021/02/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
